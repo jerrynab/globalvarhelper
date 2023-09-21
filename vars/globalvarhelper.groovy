0904d98bd5c1130963829ae041053cd6ee3e5674
jerrynab <111966274+jerrynab@users.noreply.github.com>
2023-09-21 13:18:53 +0300

    Add files via upload

---
+++ vars/globalvarhelper.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+def updateGlobalEnvVariable(String name, String value) {
+    def globalNodeProperties = Jenkins.getInstance().getGlobalNodeProperties()
+    def envVarsNodePropertyList = globalNodeProperties.getAll(hudson.slaves.EnvironmentVariablesNodeProperty.class)
+
+    if (envVarsNodePropertyList == null || envVarsNodePropertyList.size() == 0) {
+        def envVarsNodePropertyClass = this.class.classLoader.loadClass('hudson.slaves.EnvironmentVariablesNodeProperty')
+        globalNodeProperties.add(envVarsNodePropertyClass.newInstance())
+    }
+    envVarsNodePropertyList.get(0).getEnvVars().put(name, value)
+}
